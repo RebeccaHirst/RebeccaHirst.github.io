cb419f2f3192725a9b7ae863b72a5f8d6dd7dbf0
RebeccaHirst <30597180+RebeccaHirst@users.noreply.github.com>
2021-01-02 00:21:47 +0000

    correct carousel dot indexing

---
+++ main.js
@@ -0,0 +1,11 @@
+        $("#team-carousel").owlCarousel({
+            items : 3,
+            loop: true,
+            nav: true
+        });
+
+        $("#works-carousel").owlCarousel({
+            items : 5,
+            center: true,
+            smartSpeed: 650
+        });
